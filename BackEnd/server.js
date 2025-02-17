3586723b01d91d9fce4267e4207a6d41c9c73e59
Patel Mark <101583558+patelms35@users.noreply.github.com>
2025-02-17 17:56:35 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,8 +1,5 @@
-require('dotenv').config()
-const app = require('./src/app')
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 10000;
 
-
-
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
-})
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
