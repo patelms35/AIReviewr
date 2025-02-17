49d5fa15e62e0d770842c9c2392cf81b59c7d31b
Patel Mark <101583558+patelms35@users.noreply.github.com>
2025-02-17 16:50:22 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,9 +1,8 @@
 require('dotenv').config()
 const app = require('./src/app')
 
-// Use the PORT from the environment, or default to 3000 for local development
-const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000')
 })
